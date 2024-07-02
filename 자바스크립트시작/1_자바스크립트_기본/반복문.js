5e59019f648b273058b36fc555f9a1e7185d4863
ahcgnoej <jinjinchachajung@naver.com>
2024-07-03 00:38:20 +0900

    0703

--- 자바스크립트시작/1_자바스크립트_기본/반복문.js
+++ 자바스크립트시작/1_자바스크립트_기본/반복문.js
@@ -1,1 +1,23 @@
-console.log('복문')
+let num=5
+while(num){
+    console.log(num)
+    num--
+}
+
+switch(num){
+    case 1:
+        console.log('거짓')
+        break;
+    case 2:
+        console.log('거짓')
+        break;
+    case 5:
+        console.log('거짓')
+        break;
+    case 0:
+        console.log('참이다. 이미 위에서 num은 0이 되었다')
+        break;
+    case '0':
+        console.log('문자형이라 참이아니지롱')
+        break;
+}
